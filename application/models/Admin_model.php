2516f34a590b942c2626986d978d58f562c1a97c
Satelliting <jordanallen1332@gmail.com>
2019-09-29 21:25:21 -0400

    Added complete functionality.

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4,15 +4,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	class Admin_model extends CI_model{
 
 
-		# Create User Log Function
-		public function getUsers(){
-			$userList = array();
+		# Get User(s) Info Model
+		public function getUsers($userID = NULL){
+			$userInfo = NULL;
 
-			$getSQL = "SELECT * FROM users";
+			if ($userID == NULL){
+				$getSQL = "SELECT * FROM users";
+			}
+			else {
+				$getSQL = "SELECT * FROM users WHERE userid='{$userID}'";
+			}
+
 			$queryDB = $this->db->query($getSQL);
-			$userList = $queryDB->result();
+			$userInfo = $queryDB->result();
+			return $userInfo;
+		}
 
-			return $userList;
+
+
+		# Change Edited User's Info Model
+		public function changeUserInfo($userInfo){
+			$userCheck = $this->user_model->userVerifyID($userInfo['userID']);
+			if (!$userCheck){
+				return false;
+			}
+			$userChange = $this->user_model->userEdit($userInfo, true);
+			return $userChange;
+		}
+
+
+
+		# Delete User Model
+		public function deleteUser($userID){
+			$dbCheck = $this->db->delete('users', array('userID' => $userID));
+			if($dbCheck){
+				return true;
+			}
+			return false;
 		}
 
 
